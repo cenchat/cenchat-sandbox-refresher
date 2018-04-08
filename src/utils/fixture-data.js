17cee8f6a8c88a16e1c059d3f849b6dd4b894f6c
Mikko Paderes <rmmpaderes@gmail.com>
2018-04-09 01:23:44 +0800

    Update fixture data

--- src/utils/fixture-data.js
+++ src/utils/fixture-data.js
@@ -44,6 +44,42 @@ module.exports = function fixtureData(db) {
         },
       },
 
+      facebookIds: {
+        __doc__: {
+          facebookid_a: {
+            cloudFirestoreReference: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+          },
+        },
+      },
+
+      notifications: {
+        __doc__: {
+          notification_a: {
+            dataMessage: null,
+            displayMessage: {
+              body: null,
+              title: 'Test User 2 started following you',
+            },
+            createdOn: new Date('2017-01-01'),
+            type: 'follow',
+            from: db.collection('users').doc('p6dbUKSNnrh4QI4MDuZCMOs07Vt2'),
+            to: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+          },
+
+          notification_b: {
+            dataMessage: null,
+            displayMessage: {
+              body: null,
+              title: 'Test User 1 started following you',
+            },
+            createdOn: new Date('2017-01-01'),
+            type: 'follow',
+            from: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+            to: db.collection('users').doc('p6dbUKSNnrh4QI4MDuZCMOs07Vt2'),
+          },
+        },
+      },
+
       pages: {
         __doc__: {
           site_a__page_a: {
@@ -87,16 +123,50 @@ module.exports = function fixtureData(db) {
         },
       },
 
+      userMetaInfos: {
+        __doc__: {
+          QA8FnqTTt2NpIKl2VDHWqYwQKvQ2: {
+            facebookAccessToken: '12345',
+            hasNewNotification: false,
+          },
+        },
+      },
+
+      usernames: {
+        __doc__: {
+          username_a: {
+            cloudFirestoreReference: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+          },
+        },
+      },
+
       users: {
         __doc__: {
           QA8FnqTTt2NpIKl2VDHWqYwQKvQ2: {
             displayName: 'Test User 1',
-            displayUsername: null,
-            facebookId: null,
-            photoUrl: null,
-            username: null,
+            displayUsername: 'username_a',
+            facebookId: 'facebookid_a',
+            photoUrl: 'test_user1.jpg',
+            username: 'username_a',
 
             __collection__: {
+              followingComments: {
+                __doc__: {
+                  comment_b: {
+                    attachments: null,
+                    author: db.collection('users').doc('p6dbUKSNnrh4QI4MDuZCMOs07Vt2'),
+                    createdOn: new Date('2017-01-01'),
+                    isAskMeAnything: false,
+                    isDeleted: false,
+                    page: db.collection('pages').doc('site_a__page_a'),
+                    replyTo: db.collection('comments').doc('comment_a'),
+                    root: db.collection('comments').doc('comment_a'),
+                    taggedEntities: null,
+                    text: 'Hello World',
+                  },
+                },
+              },
+
               followers: {
                 __doc__: {
                   p6dbUKSNnrh4QI4MDuZCMOs07Vt2: {
@@ -134,6 +204,36 @@ module.exports = function fixtureData(db) {
             username: null,
 
             __collection__: {
+              followingComments: {
+                __doc__: {
+                  comment_a: {
+                    attachments: null,
+                    author: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+                    createdOn: new Date('2017-01-01'),
+                    isAskMeAnything: false,
+                    isDeleted: false,
+                    page: db.collection('pages').doc('site_a__page_a'),
+                    replyTo: null,
+                    root: null,
+                    taggedEntities: null,
+                    text: 'Foobar',
+                  },
+
+                  comment_c: {
+                    attachments: [{ id: 'sticker_a1', type: 'sticker' }],
+                    author: db.collection('users').doc('QA8FnqTTt2NpIKl2VDHWqYwQKvQ2'),
+                    createdOn: new Date('2017-01-01'),
+                    isAskMeAnything: false,
+                    isDeleted: false,
+                    page: db.collection('pages').doc('site_a__page_a'),
+                    replyTo: db.collection('comments').doc('comment_b'),
+                    root: db.collection('comments').doc('comment_a'),
+                    taggedEntities: null,
+                    text: null,
+                  },
+                },
+              },
+
               followers: {
                 __doc__: {
                   QA8FnqTTt2NpIKl2VDHWqYwQKvQ2: {
