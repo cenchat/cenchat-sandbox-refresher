8d01890a31657c814aac427ab65fd7b42a4adceb
Mikko Paderes <rmmpaderes@gmail.com>
2018-09-08 19:30:16 +0800

    Update fixture data

--- src/utils/test-accounts.js
+++ src/utils/test-accounts.js
@@ -12,5 +12,29 @@ module.exports = function testAccounts() {
       password: 'user_b',
       displayName: 'User B',
     },
+    {
+      uid: 'user_C',
+      email: 'user_C@gmail.com',
+      password: 'user_C',
+      displayName: 'User C',
+    },
+    {
+      uid: 'user_d',
+      email: 'user_d@gmail.com',
+      password: 'user_d',
+      displayName: 'User D',
+    },
+    {
+      uid: 'user_e',
+      email: 'user_e@gmail.com',
+      password: 'user_e',
+      displayName: 'User E',
+    },
+    {
+      uid: 'user_f',
+      email: 'user_f@gmail.com',
+      password: 'user_f',
+      displayName: 'User F',
+    },
   ];
 };
